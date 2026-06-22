0ca18dc96ec11d609f80c34b1c40e12397fce19d
Dani <daniellytoledosouto@gmail.com>
2026-06-22 11:29:45 +0100

    bd cidades

---
+++ app/models/rep_cidades.py
@@ -0,0 +1,15 @@
+from app.bd_config import conectar_pymysql
+# from pprint import pprint
+
+def select_todas_cidades():
+    conexao = conectar_pymysql()
+    cursor = conexao.cursor()
+
+    sql = "SELECT * FROM cidades"
+    cursor.execute(sql)
+    resultado = cursor.fetchall()
+
+    return(resultado)
+
+# if __name__ == "__main__":
+#   pprint(select_todas_cidades())
---
+++ app/services/srv_cidades.py
@@ -0,0 +1,4 @@
+from app.models.rep_cidades import select_todas_cidades
+
+def lista_cidades():
+    return select_todas_cidades()
