63043465513955518199cecb31956f996241efc3
Dani <daniellytoledosouto@gmail.com>
2026-06-25 10:11:29 +0100

    adicionando img em srv

--- app/services/srv_cidades.py
+++ app/services/srv_cidades.py
@@ -1,4 +1,4 @@
-from app.models.rep_cidades import select_todas_cidades, select_cidade
+from app.models.rep_cidades import select_todas_cidades, select_cidade, select_imagens_cidade
 from pprint import pprint
 
 def lista_cidades():
@@ -8,8 +8,10 @@ def detalhes_cidade(cidade_id):
     dados = select_cidade(cidade_id)
     if int(dados['dataf_c']) < 0:
         ano = str(abs(dados['dataf_c'])) + " AC"
-        dados['dataf_c'] = ano 
-    return dados
+        dados['dataf_c'] = ano
+    fotos = select_imagens_cidade(cidade_id)
+    dados_finais = [dados, fotos]     
+    return dados_finais
 
 if __name__ == "__main__":
     pprint(detalhes_cidade(1))
